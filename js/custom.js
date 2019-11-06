edd3f0e8e9dfcad4b13777810ff4e332498d2d00
unknown <theoliao1998@gmail.com>
2019-11-06 11:28:11 -0500

    update

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
 	$("body").css("display", "none");
 	$("body").fadeIn(1000);
 	$(".occupy").css("height", $("footer").height());
+	$("#bottom").css("height", $(window).height()-$(".occupy").height()-$("#main").height());
+	// $("li").css("line-height",$("a.page").height());
 
-	$("a.page").click(function(event){
+	$("a.page,a.flip-card-back").click(function(event){
 		event.preventDefault();
 		linkLocation = this.href;
 		$("body").fadeOut(500, redirectPage);		
@@ -15,6 +17,14 @@ $(document).ready(function() {
 	}
 	
 });
+
+// function resizeTitle(){
+// 	$(".page").quickfit();
+// }
+
+// $(document).ready(resizeTitle);
+// $(window).resize(resizeTitle);
+
 
 
 $("*").focus(function() {
--- js/gallary.js
+++ js/gallary.js
@@ -3,8 +3,14 @@ $(document).ready(resize);
 $(window).resize(resize);
 
 function resize(){
-    $("#image").css("height", $(window).height()-$(".navigation").height()-$("footer").height());
-    $("#image").css("width", $(window).width()-$(".preview_gallary").width());
+    
+    if($(window).width()>$(window).height()){
+        $("#image").css("width", $(window).width()-3*$(".preview").width());
+        $("#image").css("height", $(window).height()-$(".navigation").height()-$("footer").height());
+    } else{
+        $("#image").css("width",$(window).width());
+        $("#image").css("height",$(window).width());
+    }
     $(".preview").css("width", $(".preview").height());
 }
 
